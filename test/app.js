1d51495daf3b4c084e26b582f7c4545afa32bd1b
Andrew Madden <amadden80@gmail.com>
2018-06-05 09:11:37 -0400

    introduce sampling, filtering, and defaults

--- test/app.js
+++ test/app.js
@@ -1,13 +1,23 @@
-var bt = require('../lib/index');
+var bt = require("../lib/index");
 bt.initialize({
   token: "my test token",
-  endpoint: "http://localhost:11369",
+  endpoint: "http://localhost:11369"
+  // sampling: 0.25,  // Should send 25/100 caught errors
+  // filter: function(report) {
+  //   if (report.attributes["error.message"] == "Script Error.") {
+  //     var shouldSend = Math.random() >= 0.5;
+  //     return shouldSend;
+  //   }
+  //   return true;
+  // }
 });
 
-var btnDom = document.getElementById('error-button');
+bt.report(new Error("Script Error."));
 
-btnDom.addEventListener('click', onBtnClick, false);
+var btnDom = document.getElementById("error-button");
+
+btnDom.addEventListener("click", onBtnClick, false);
 
 function onBtnClick(ev) {
-  badFnCall()
+  badFnCall();
 }
