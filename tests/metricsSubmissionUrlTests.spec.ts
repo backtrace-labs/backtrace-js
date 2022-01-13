8eb63684c237a6152a51a6d2bd09178b5986b59f
Stone <mfollari@backtrace.io>
2022-01-13 11:21:14 -0800

    add metrics submission url tests for on premise urls

--- tests/metricsSubmissionUrlTests.spec.ts
+++ tests/metricsSubmissionUrlTests.spec.ts
@@ -39,4 +39,52 @@ describe('Test Submission URL information in metrics support', () => {
 
     expect(params).toBeUndefined();
   });
+
+  it('Test valid on-premise <universe>.backtrace.io url with configuration token', () => {
+    const expectedUniverseName = 'yolo';
+    const expectedToken = '55555111112345eb7ae344f6e002de2e20c81fbdedf6991c2f3bb45b32b55555'
+    const testSubmissionUrl = `https://${expectedUniverseName}.backtrace.io/`;
+
+    const params = getEndpointParams(testSubmissionUrl, expectedToken);
+    if (!params) {
+      throw new Error(
+        `Invalid test case - the test submission URL and ${testSubmissionUrl} and token ${expectedToken} are invalid for current test`,
+      );
+    }
+
+    expect(params.universe).toBe(expectedUniverseName);
+    expect(params.token).toBe(expectedToken);
+  });
+
+  it('Test valid on-premise <universe>.sp.backtrace.io url with configuration token', () => {
+    const expectedUniverseName = 'yolo-with-sp';
+    const expectedToken = '55555111112345eb7ae344f6e002de2e20c81fbdedf6991c2f3bb45b32b55555'
+    const testSubmissionUrl = `https://${expectedUniverseName}.sp.backtrace.io/`;
+
+    const params = getEndpointParams(testSubmissionUrl, expectedToken);
+    if (!params) {
+      throw new Error(
+        `Invalid test case - the test submission URL and ${testSubmissionUrl} and token ${expectedToken} are invalid for current test`,
+      );
+    }
+
+    expect(params.universe).toBe(expectedUniverseName);
+    expect(params.token).toBe(expectedToken);
+  });
+
+  it('Test valid on-premise <universe>.sp.backtrace.io url with undefined configuration token', () => {
+    const expectedUniverseName = 'yolo-with-sp';
+    const expectedToken = undefined;
+    const testSubmissionUrl = `https://${expectedUniverseName}.sp.backtrace.io/`;
+
+    const params = getEndpointParams(testSubmissionUrl, expectedToken);
+    if (!params) {
+      throw new Error(
+        `Invalid test case - the test submission URL and ${testSubmissionUrl} and token ${expectedToken} are invalid for current test`,
+      );
+    }
+
+    expect(params.universe).toBe(expectedUniverseName);
+    expect(params.token).toBe(expectedToken);
+  });
 });
